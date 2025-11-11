1db5f62cca51714cffef6bb892acada3ebc17556
lorenzoramong <lorenzoramong@gmail.com>
2025-11-11 16:22:10 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+<div style={{
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
+      padding: '50px'
+    }}>
+<h1>📘 Línea de Tiempo de Facturación</h1>
+<p>Esta será la app interna para registrar los hitos históricos.</p>
+</div>
+  );
+}
